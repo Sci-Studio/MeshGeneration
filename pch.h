409d57bed12fb918fb9bcd7f9914181db6180b2f
HishamSaeed <hisham.saeed.92@gmail.com>
2025-03-25 02:11:33 +0100

    fix pch for clean build

--- pch.h
+++ pch.h
@@ -1,9 +1,12 @@
 #ifndef PCH_H
 #define PCH_H
 
+
+#include <QOpenGLFunctions_4_2_Compatibility>
+
 // Define the OpenGL header macro
-#define QOPENGL_HEADER <QOpenGLFunctions_4_2_Compatibility>
-#include QOPENGL_HEADER
+//#define QOPENGL_HEADER <QOpenGLFunctions_4_2_Compatibility>
+//#include QOPENGL_HEADER
 
 // Define an alias for the OpenGL class
 using QOpenGLClass = QOpenGLFunctions_4_2_Compatibility;
